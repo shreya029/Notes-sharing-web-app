cb8659bc0217026eb32b159618b9adbae61cf658
Dnyanashree Mutalik <dnyanashreemutalik@gmail.com>
2026-05-17 22:17:45 +0530

    updated config fiel

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -9,13 +9,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCmQjLXwoWepC5oN6oWJv6OSuNmxVmEC9A",
-  authDomain: "notes-sharing-app-ff78b.firebaseapp.com",
-  projectId: "notes-sharing-app-ff78b",
-  storageBucket: "notes-sharing-app-ff78b.firebasestorage.app",
-  messagingSenderId: "419464425108",
-  appId: "1:419464425108:web:d5215e14889ceb1bf7ce81",
-  measurementId: "G-TRS7W4J3FQ"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
